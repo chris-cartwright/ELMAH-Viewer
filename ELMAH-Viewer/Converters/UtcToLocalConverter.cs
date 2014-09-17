151f1144b12192df33088a157e4340d07adc65b4
chris-cartwright <cartwright.christopher@gmail.com>
2014-09-16 21:18:31 -0400

    Error logs display in local time

---
+++ ELMAH-Viewer/Converters/UtcToLocalConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ELMAH_Viewer.Converters
+{
+	public class UtcToLocalConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value.GetType() != typeof(DateTime))
+			{
+				throw new NotSupportedException();
+			}
+
+			DateTime dt = (DateTime)value;
+			return dt.ToLocalTime();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
